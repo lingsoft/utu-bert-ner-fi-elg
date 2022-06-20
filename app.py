8067df8674fc0703df5290ea307b08344a43069e
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-06-20 16:06:26 +0300

    Improve error handling

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from keras_bert_ner.serve import Tagger
 from utils import iob2_to_elg
 
 MODEL_DIR = 'keras_bert_ner/ner-model'
+MAX_CHAR = 15000
 
 
 class FinBertNer(FlaskService):
@@ -14,20 +15,29 @@ class FinBertNer(FlaskService):
 
     def convert_to_elg(self, content, ner_res):
         annotations = {}
-        entities = iob2_to_elg(content, ner_res)
-        for entity in entities:
-            label, annot = entity
-            annotations.setdefault(label, []).append(annot)
         try:
+            entities = iob2_to_elg(content, ner_res)
+            for entity in entities:
+                label, annot = entity
+                annotations.setdefault(label, []).append(annot)
             return AnnotationsResponse(annotations=annotations)
         except Exception as err:
             error_msg = StandardMessages.\
                     generate_elg_service_internalerror(params=[str(err)])
             return Failure(errors=[error_msg])
 
-    def process_text(self, content):
-        ner_res = self.tagger.tag(content.content, False)
-        return self.convert_to_elg(content.content, ner_res)
+    def process_text(self, request: TextRequest):
+        content = request.content
+        if len(content) > MAX_CHAR:
+            error_msg = StandardMessages.generate_elg_request_too_large()
+            return Failure(errors=[error_msg])
+        try:
+            ner_res = self.tagger.tag(content, False)
+        except ValueError as err:
+            error_msg = StandardMessages.\
+                    generate_elg_service_internalerror(params=[str(err)])
+            return Failure(errors=[error_msg])
+        return self.convert_to_elg(content, ner_res)
 
 
 flask_service = FinBertNer("FinBERTNER")
